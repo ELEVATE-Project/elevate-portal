bd8a9c4d54240a2265be061db153bf6da5ec5250
namita-25 <namita_g@tekditechnologies.com>
2026-04-03 13:49:28 +0530

    4664 [R3.4.0 SAAS_QA_UR] App icon logo appears zoomed in PWA

--- apps/shikshagraha-app/src/app/manifest.ts
+++ apps/shikshagraha-app/src/app/manifest.ts
@@ -38,7 +38,7 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         src: logo,
         sizes: '512x512',
         type: iconType,
-        purpose: 'maskable',
+        purpose: 'any',
       },
     ],
   };
